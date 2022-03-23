ef78865eec831025cfede6ff508e525bf929d90a
Aditi Roy <aditiroy.21.ar@gmail.com>
2022-03-23 22:42:13 +0530

    routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,19 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen.js'
 import ProductScreen from './screens/ProductScreen.js'
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <main className='py-3'>
-        <Container>
-          <Route path='/' component={HomeScreen} exact />
-          <Route path='/product/:id' component={ProductScreen} />
-        </Container>
-      </main>
-      <Footer />
-    </Router>
+    <BrowserRouter>
+       <Routes>
+          <Route path='/' exact element={<HomeScreen/>} />
+          {/* <Route path='/product/:id' component={ProductScreen} /> */}
+       </Routes>
+    </BrowserRouter>
   )
 }
 
