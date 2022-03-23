cd13d09c43f08122a85c05b6eabbf04f96a39f0e
jishu-yadav <jishuyadavcute88@gmail.com>
2022-03-23 15:28:12 +0530

    Backend Routes

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,36 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import colors from 'colors'
-import connectDB from './config/db.js'
+// import express from 'express'
+const express = require('express')
+// import dotenv from 'dotenv'
+// import colors from 'colors'
+// import connectDB from './config/db.js'
 
-dotenv.config()
+const products = require('./data/products')
 
-connectDB()
+// dotenv.config()
+// connectDB()
 
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('API is running....')
+  res.send('API is running....')
 })
 
-const PORT = process.env.PORT || 5000
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
 
-app.listen(
-  PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold.underline
-  )
-)
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id)
+  res.json(product)
+})
+
+app.listen(5000, console.log('Server is running on port 5000'))
+
+// const PORT = process.env.PORT || 5000
+
+// app.listen(
+//   PORT,
+//   console.log(
+//     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold.underline
+//   )
+// )
