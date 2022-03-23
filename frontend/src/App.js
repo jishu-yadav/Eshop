a736cc957fb49406e1a3089aace48c84208984cb
jishu-yadav <jishuyadavcute88@gmail.com>
2022-03-23 11:25:31 +0530

    HomeScreen products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 function App() {
   return (
     <div>
       <Header />
       <main className='py-3'>
         <Container>
+          <HomeScreen />
           <h1>Welcome to Ready Online!</h1>
         </Container>
       </main>
