00b85db6140dd642f5a4c5045c3ee45a8e8f88fd
LAPTOP-DQGO07IU\Asus <aparna232001@gmail.com>
2022-03-23 22:10:56 +0530

    Starting The Front End

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,26 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
+import HomeScreen from './screens/HomeScreen.js'
+import ProductScreen from './screens/ProductScreen.js'
+
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
-          <h1>Welcome to Ready Online!</h1>
+        <h1>Welcome to Ready Online!</h1>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+           
+          
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   )
 }
 
