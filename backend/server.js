0bd1b8fed9175df762335707192f5468247be8e0
jishu-yadav <jishuyadavcute88@gmail.com>
2022-03-24 14:04:14 +0530

    jishu-localhost

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,16 @@
-// import express from 'express'
-const express = require('express')
-// import dotenv from 'dotenv'
-// import colors from 'colors'
-// import connectDB from './config/db.js'
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
+import products from './data/products.js'
 
-const products = require('./data/products')
-
-// dotenv.config()
-// connectDB()
+dotenv.config()
+connectDB()
 
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('API is running....')
+  res.send('API is running...')
 })
 
 app.get('/api/products', (req, res) => {
@@ -24,9 +22,13 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log('Server is running on port 5000'))
-
-// const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
 
 // app.listen(
 //   PORT,
