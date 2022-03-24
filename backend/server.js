2ad414a231bf82d0e520b6097332a87e34c8c7ee
jishu-yadav <jishuyadavcute88@gmail.com>
2022-03-24 22:51:18 +0530

    prepared sample data , seeder script run , fetched products from databse

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,23 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 
+import productRoutes from './routes/productRoutes.js'
+
 dotenv.config()
 
 connectDB()
-
 const app = express()
-
 app.get('/', (req, res) => {
-    res.send('API is running....')
+  res.send('API is running....')
 })
+
+app.use('/api/products', productRoutes)
 
 const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold.underline
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
 )
