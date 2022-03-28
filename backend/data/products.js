2bda2b84cc1cc9f9afe1ea6f690e834f4d38cab0
Aparna Raj <aparna232001@gmail.com>
2022-03-29 00:17:40 +0530

    product images added

--- backend/data/products.js
+++ backend/data/products.js
@@ -6,47 +6,13 @@ const products = [
       'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
     brand: 'Apple',
     category: 'Electronics',
-    price: 89.99,
+    price: 10000,
     countInStock: 10,
     rating: 4.5,
     numReviews: 12,
   },
-  {
-    name: 'iPhone 11 Pro 256GB Memory',
-    image: '/images/phone.jpg',
-    description:
-      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
-    brand: 'Apple',
-    category: 'Electronics',
-    price: 599.99,
-    countInStock: 7,
-    rating: 4.0,
-    numReviews: 8,
-  },
-  {
-    name: 'Cannon EOS 80D DSLR Camera',
-    image: '/images/camera.jpg',
-    description:
-      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
-    brand: 'Cannon',
-    category: 'Electronics',
-    price: 929.99,
-    countInStock: 5,
-    rating: 3,
-    numReviews: 12,
-  },
-  {
-    name: 'Sony Playstation 4 Pro White Version',
-    image: '/images/playstation.jpg',
-    description:
-      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
-    brand: 'Sony',
-    category: 'Electronics',
-    price: 399.99,
-    countInStock: 11,
-    rating: 5,
-    numReviews: 12,
-  },
+  
+
   {
     name: 'Logitech G-Series Gaming Mouse',
     image: '/images/mouse.jpg',
@@ -54,23 +20,77 @@ const products = [
       'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
     brand: 'Logitech',
     category: 'Electronics',
-    price: 49.99,
+    price: 1995,
     countInStock: 7,
-    rating: 3.5,
+    rating: 4.0,
     numReviews: 10,
   },
+  
   {
-    name: 'Amazon Echo Dot 3rd Generation',
-    image: '/images/alexa.jpg',
+    name: 'Sapiens: A Brief History of Humankind : Yuval Noah Harari',
+    image: '/images/sapiens.png',
     description:
-      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
-    brand: 'Amazon',
-    category: 'Electronics',
-    price: 29.99,
-    countInStock: 0,
-    rating: 4,
-    numReviews: 12,
+      'In Sapiens, Dr Yuval Noah Harari spans the whole of human history, from the very first humans to walk the earth to the radical – and sometimes devastating – breakthroughs of the Cognitive, Agricultural and Scientific Revolutions.',
+    brand: 'Vintage',
+    category: 'Books',
+    price: 700,
+    countInStock: 10,
+    rating: 4.6,
+    numReviews: 18,
   },
+
+  {
+    name: 'Air Jordan 7 Retro OC sneakers',
+    image: '/images/sneakers.jpg',
+    description:
+      'The Miro Olympic colorway of the Air Jordan 7 was inspired by the Dona i Ocell sculpture in Barcelona. A black base creates a suede underlay, and white overlays are coated in a unique red, blue, green, and yellow design',
+    brand: 'Jordan',
+    category: 'Footwear',
+    price: 20000,
+    countInStock: 8,
+    rating: 4,
+    numReviews: 11,
+  },
+
+  {
+    name: 'Gucci Bamboo type Perfume',
+    image: '/images/gucci-bamboo.jpg',
+    description:
+      "Gucci Bamboo, a new fragrance for today's multi-faceted woman soft and intense at once, blooming with notes of exotic Casablanca lily and Tahitian vanilla.",
+    brand: 'Gucci',
+    category: 'Cosmetics',
+    price: 300,
+    countInStock: 9,
+    rating: 4.3,
+    numReviews: 15,
+  },
+
+  {
+    name: 'N2-X Back Pack with Zip Closure',
+    image: '/images/backpack.jpg',
+    description:
+      'Coil / Plastic / Metal Zipper Direct Deliver from SBS Zipper. High Quality. Supply Backpack/Bag Zipper.',
+    brand: 'N2-X',
+    category: 'Backpacks',
+    price: 2000,
+    countInStock: 7,
+    rating: 4,
+    numReviews: 16,
+  },
+
+  {
+    name: 'Vault UK-2000C 23-Inch Solid Mahogany Top Premium Concert Ukulele With Gigbag',
+    image: '/images/ukulele.jpg',
+    description:
+      'The Vault UK-2000C is a 23-Inch Solid Mahogany Top - Soprano Ukulele, with Mahogany laminate back and sides. Solid wood-top ukuleles have a distinct advantage over laminate or ply tops in the quality of resonance that they provide. This is the perfect ukulele for any learning to advanced musician and is lightweight, comfortable & compact.',
+    brand: 'Amazon',
+    category: 'Instruments',
+    price: 5000,
+    countInStock: 10,
+    rating: 4.2,
+    numReviews: 14,
+  },
+
 ]
 
 export default products
