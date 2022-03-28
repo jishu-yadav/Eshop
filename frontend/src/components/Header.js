679d02874a7f7e4beed9d600f9750d5a98dcc82e
jishu-yadav <jishuyadavcute88@gmail.com>
2022-03-29 01:06:54 +0530

    !

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>Ready Online!</Navbar.Brand>
+            <Navbar.Brand>Ready Online!!</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
