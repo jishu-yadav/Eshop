fa01d8df6fd02db2bb77620dfdae196358ff39ce
Aditi Roy <aditiroy.21.ar@gmail.com>
2022-03-29 00:54:20 +0530

    added my email to users

--- backend/data/users.js
+++ backend/data/users.js
@@ -17,6 +17,11 @@ const users = [
     email: 'jane@example.com',
     password: bcrypt.hashSync('123456', 10),
   },
+  {
+    name: 'Aditi',
+    email: 'aditiroy.21.ar@gmail.com',
+    password: bcrypt.hashSync('8520', 10),
+  },
 ]
 
 export default users
